d4c1851a2e213f31bb7bae9c4eeefda29cd733d1
lilinsong3 <imlilinsong@163.com>
2023-11-04 15:48:08 +0800

    update MediaLibraryService

--- app/src/main/java/com/github/lilinsong3/m3player/ui/play/AudioLibraryService.kt
+++ app/src/main/java/com/github/lilinsong3/m3player/ui/play/AudioLibraryService.kt
@@ -1,11 +1,15 @@
 package com.github.lilinsong3.m3player.ui.play
 
+import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.LibraryResult
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession
+import com.google.common.collect.ImmutableList
+import com.google.common.util.concurrent.ListenableFuture
 
 class AudioLibraryService: MediaLibraryService() {
-    var audioLibrarySession: MediaLibrarySession? = null
+    private var audioLibrarySession: MediaLibrarySession? = null
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaLibrarySession? =
         audioLibrarySession
 
@@ -14,8 +18,7 @@ class AudioLibraryService: MediaLibraryService() {
         audioLibrarySession = MediaLibrarySession.Builder(
             this,
             ExoPlayer.Builder(this).build(),
-            // TODO: add a impl
-            object : MediaLibrarySession.Callback{}
+            AudioLibrarySessionCallback()
         ).build()
     }
 
@@ -23,8 +26,77 @@ class AudioLibraryService: MediaLibraryService() {
         audioLibrarySession?.run {
             player.release()
             release()
+            // listener for service should be cleared if setListener() for service is called
+            // clearListener()
             audioLibrarySession = null
         }
         super.onDestroy()
+    }
+
+    // TODO: Impl this
+    class AudioLibrarySessionCallback: MediaLibrarySession.Callback {
+        override fun onGetLibraryRoot(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<MediaItem>> {
+            return super.onGetLibraryRoot(session, browser, params)
+        }
+
+        override fun onGetItem(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            mediaId: String
+        ): ListenableFuture<LibraryResult<MediaItem>> {
+            return super.onGetItem(session, browser, mediaId)
+        }
+
+        override fun onGetChildren(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            parentId: String,
+            page: Int,
+            pageSize: Int,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<ImmutableList<MediaItem>>> {
+            return super.onGetChildren(session, browser, parentId, page, pageSize, params)
+        }
+
+        override fun onSubscribe(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            parentId: String,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<Void>> {
+            return super.onSubscribe(session, browser, parentId, params)
+        }
+
+        override fun onUnsubscribe(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            parentId: String
+        ): ListenableFuture<LibraryResult<Void>> {
+            return super.onUnsubscribe(session, browser, parentId)
+        }
+
+        override fun onSearch(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            query: String,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<Void>> {
+            return super.onSearch(session, browser, query, params)
+        }
+
+        override fun onGetSearchResult(
+            session: MediaLibrarySession,
+            browser: MediaSession.ControllerInfo,
+            query: String,
+            page: Int,
+            pageSize: Int,
+            params: LibraryParams?
+        ): ListenableFuture<LibraryResult<ImmutableList<MediaItem>>> {
+            return super.onGetSearchResult(session, browser, query, page, pageSize, params)
+        }
     }
 }
--- app/src/main/java/com/github/lilinsong3/m3player/ui/play/PlayFragment.kt
+++ app/src/main/java/com/github/lilinsong3/m3player/ui/play/PlayFragment.kt
@@ -1,30 +1,59 @@
 package com.github.lilinsong3.m3player.ui.play
 
+import android.content.ComponentName
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.github.lilinsong3.m3player.databinding.FragmentPlayBinding
+import androidx.media3.session.MediaBrowser
+import androidx.media3.session.SessionToken
+import com.github.lilinsong3.m3player.databinding.TestBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class PlayFragment : Fragment() {
 
-    private var _binding: FragmentPlayBinding? = null
+    private var _binding: TestBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: PlayViewModel by viewModels()
+
+    private lateinit var browserFuture: ListenableFuture<MediaBrowser>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPlayBinding.inflate(inflater, container, false)
+        _binding = TestBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        val context = requireContext()
+        browserFuture = MediaBrowser.Builder(
+            context,
+            SessionToken(context, ComponentName(context, AudioLibraryService::class.java))
+        ).buildAsync()
+        browserFuture.addListener(
+            {
+                binding.testPlayerView.player = browserFuture.get()
+            },
+            // MoreExecutors.directExecutor()
+            ContextCompat.getMainExecutor(context)
+        )
+    }
+
     override fun onDestroyView() {
-        super.onDestroyView()
+        binding.testPlayerView.player = null
         _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onStop() {
+        MediaBrowser.releaseFuture(browserFuture)
+        super.onStop()
     }
 }
